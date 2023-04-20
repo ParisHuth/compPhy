c9338233f1cfb9423814647b110ce1d437c55153
ParisHuth <paris.huth2001@gmail.com>
2023-04-20 10:58:46 +0200

    adding rough documentation

--- work/ex01.R
+++ work/ex01.R
@@ -1,17 +1,29 @@
 # Paris J. Huth: Gruppe 1
 # Q inich Pakal Figueroa Coc: Gruppe 5
 
+
+# Numerical Integration
+## a
+
+# defining the integrand as a function
 inte <- function(x,a,n){
     x^n/(x+a)
 }
+
+# assigning variables
 a <- 5
 x <- seq(0,1,length.out=100)
 n <- c(1,5,10,20,30,50)
+
+# computing the integrand
 y <- lapply(n, function(i) inte(x,a,i))
 
+# plotting every configuration
 plot(x,y[[1]],type='l',ylab='y')
 colors <- rainbow(length(n))
 for (i in seq_along(n)){
     lines(x,y[[i]],col=colors[i])
 }
 legend('topleft',legend=n, col = colors,lty=1)
+
+## b
