ff98e9cd309558581b6586622307368deb0dc468
ParisHuth <paris.huth2001@gmail.com>
2023-05-03 22:37:42 +0200

    implementing euler in the homework, todo energie and b)

--- work/ex02.R
+++ work/ex02.R
@@ -2,90 +2,82 @@
 # Q inich Pakal Figueroa Coc: Gruppe 5
 # Introduction to computation physics ex. 2
 
-# # initial values
-# G <- 1
-# M1 <- M2 <- 1
-# r <- 1
-# v <- 1
-# t <- 1
-# ep <- 1
-# 
-# # characteristic scale
-# R <- r
-# V <- sqrt(2/R)
-# T <- R/V
-# e <- (r*v^2-(r*v)*v)/(G*(M1+M2))-r/sqrt(sum(r^2))
-
-# tutorial stuff
-# tried to translate into r may need some serious troubeshooting
-e_tot <- function(S,W){
-    W2 <- sum(W*W)
-    S2 <- sum(S*S)
-    return(0.5*W2-1/sqrt(S2))
-}
-
-rl_vec <- function(S,W){
-    W2 <- sum(W*W)
-    S2 <- sum(S*S)
-    SW <- sum(S*W)
-    return(S*W2 -SW*W-S*1/sqrt(S2))
-}
-
-T_orbits <- function(S,W){
-    a <- -1/e_tot(S,W)
-    return(pi*a*sqrt(a/2))
-}
-
-retrieve_trajectories <- function(m1,m2,S,r0){
-    Xi1 <- m2/(m2+m1)*S*r0*2
-    Xi2 <- -m1/(m2+m1)*S*r0*2
-    return(c(Xi1,Xi2))
-}
-
-accel <- function(sn){
-    s_sq <- sum(sn*sn)
-    s <- sqrt(s_sq)
-    s3 <- s_sq*s
-    return(-sn/s3)
-}
-
-twoEuler <- function(s0,w0,del_tau,n){
-    S_out <- matrix(nrow = n,ncol=length(s0))
-    W_out <- matrix(nrow = n,ncol=length(w0))
-    S_out[1,] <- s0
-    W_out[1,] <- w0
-    S_np1 <- s0
-    W_np1 <- w0
-    for (n in 1:(n-1)){
-        S_n <- S_np1
-        W_n <- W_np1
-        S_np1 <- S_n+W_n*del_tau
-        W_np1 <- W_n+del_tau*accel(S_n)
-        S_out[n+1,] <- S_np1
-        W_out[n+1,] <- W_np1
+# Implementing euler scheme
+euler <- function(s0, w0, h, Tr){
+    for (i in 1:3){
+        stopifnot('Only works with data in up to three dimensions.'=length(s0)<=3)
+        stopifnot('Only works with data in up to three dimensions.'=length(w0)<=3)
+        if (length(s0) < 3){
+            s0 <- c(s0, 0)
+        }
+        if (length(w0) < 3){
+            w0 <- c(w0, 0)
+        }
+        if (length(s0) >= 3 && length(w0) >= 3){
+            break
+        }
     }
-    return(list(S_out,W_out))
+    
+    N <- Tr/h
+    temp <- numeric(N)
+    s <- data.frame(temp,temp,temp)
+    w <- s
+    s[1,] <- s0
+    w[1,] <- w0
+    
+    for (i in 2:N){
+        s[i,] <- s[i-1,]+w[i-1,]*h
+        w[i,] <- w[i-1,]-(s[i-1,]/(sum(s[i-1,]^2)^(3/2)))*h
+    }
+    
+    return(s)
 }
-
-
-### next ex
-
-m1 <- 1
-m2 <- 1
-r0 <- 1
 
 s0 <- c(1,0,0)
 w0 <- c(0,1,0)
-delta_tau <- 0.01
-T_orb <- T_orbits(s0,w0) ## TODO check for vector compadibility
-T_orb
+s4 <- euler(s0,w0,1,10)
+plot(s4[,1],s4[,2],col='green', type='l',ylim=c(-2.4,4.3))
 
-temp <- twoEuler(s0,w0,delta_tau,T_orb/delta_tau)
-S_fE <- temp[[1]]
-W_fE <- temp[[2]]
+s1 <- euler(s0,w0,0.001,10)
+lines(s1[,1],s1[,2])
 
-temp <- retrieve_trajectories(m1,m2,S_fE,r0)
-X1 <- temp[1]
-X2 <- temp[2]
-X1
-X2
+s2 <- euler(s0,w0,0.01,10)
+lines(s2[,1],s2[,2],col='red')
+
+s3 <- euler(s0,w0,0.1,10)
+lines(s3[,1],s3[,2],col='blue')
+
+
+ #different velocity
+s0 <- c(1,0,0)
+w0 <- c(0,0.6,0)
+s4 <- euler(s0,w0,1,10)
+plot(s4[,1],s4[,2],col='green',
+    type='l',ylim=c(-9.95,1.43),
+            xlim=c(-10.08,4.1))
+
+s1 <- euler(s0,w0,0.001,10)
+lines(s1[,1],s1[,2])
+
+s2 <- euler(s0,w0,0.01,10)
+lines(s2[,1],s2[,2],col='red')
+
+s3 <- euler(s0,w0,0.1,10)
+lines(s3[,1],s3[,2],col='blue')
+
+s0 <- c(1,0,0)
+w0 <- c(0,0.2,0)
+s4 <- euler(s0,w0,1,10)
+plot(s4[,1],s4[,2],col='green',
+     type='l',ylim=c(-60.2,0.42),
+     xlim=c(-12.31,21.3))
+
+s1 <- euler(s0,w0,0.001,10)
+lines(s1[,1],s1[,2])
+
+s2 <- euler(s0,w0,0.01,10)
+lines(s2[,1],s2[,2],col='red')
+
+s3 <- euler(s0,w0,0.1,10)
+lines(s3[,1],s3[,2],col='blue')
+
